316c02e2144e8c086e3af4d36b3c3e36a20a5544
Jovan Gerodetti <jovan.gerodetti@titannano.de>
2015-07-01 18:22:36 +0200

    backend entity draft

---
+++ backend/Classes/Cocktail.php
@@ -0,0 +1,37 @@
+<?php
+
+class CocktailEntity extends Entity {
+
+    private $table = 'cocktails';
+    
+    private $index = 'id';
+        
+    public function load($id){
+        $this->load($id);
+    }
+    
+    public function save(){
+        $fields = [
+            "`name`= " . $this->getName() . "'",
+            "`id` = '" . $this->getId() . "'"
+        ];
+        
+        $this->_save($this->fields['id'], $fields);
+    }
+    
+    public function getId(){
+        return $this->fields['id'];
+    }
+    
+    public function setId($value){
+        $this->fields['id'] = $value;
+    }
+    
+    public function getName(){
+        return $this->fields['name'];
+    }
+    
+    public function setName($value){
+        $this->fields['name'] = $value;
+    }
+}
--- backend/Classes/Entity.php
+++ backend/Classes/Entity.php
@@ -2,8 +2,29 @@
 
 class Entity {
 
-    public function constructor(){
+    private $table = null;
 
+    private $index = null;
+
+    private $fields = [];
+
+    public function constructor(){
+    }
+
+    private function _save($key, $fields){
+
+    }
+
+    private function _load($key){
+
+    }
+
+    private function _delete($key, $fields){
+
+    }
+
+    public function assing($fields){
+        $this->fields = $fields;
     }
 
 }
