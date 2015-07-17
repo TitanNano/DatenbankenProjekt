12e5f78532c75c5d095bdb7634ff6d1c4ab91d1a
Marco Mohr <mohr-marco1@gmx.de>
2015-07-17 09:48:46 +0200

    delete and search Entity

--- backend/Classes/ActionsMap.php
+++ backend/Classes/ActionsMap.php
@@ -15,13 +15,34 @@ class actionsMap {
     public static function getSupplier ($session)
     {
         $supplier = new SupplierCollection ();
-
-        $supplier->addFilter ("name=" .  $session['query']);
+        $supplier->addFilter("name" .  $session['query']);
     }
 
     public static function displayEntity ($session)
     {
         $entity = new $session['entityTyp']();
         $entity->load($session['entityID']);
+        return $entity->export();
+    }
+
+    public static function newEntity ($session)
+    {
+        $entity = new session['entityTyp']();
+        $entity->assign($session['entity']);
+        $entity->save();
+    }
+
+    public static function deleteEntity ($session)
+    {
+        $entity = new session['entityTyp']();
+        $entity->load($session['entity']);
+        $entity->delete();
+    }
+
+    public static function searchIngredient ($session)
+    {
+        $ingredient = new IngredientCollection ();
+        $ingredient->addFilte("name" .  $session['query']);
+        $ingredient->load();
     }
 }
